b12cb28a1eeac74bafead72cccb97c9fb5881fe9
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-04-21 11:25:52 -0600

    Solution

--- task.sql
+++ task.sql
@@ -1,15 +1,12 @@
 USE ShopDB;
-START TRANSACTION;
 
 INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-01');
 SET @orderId = LAST_INSERT_ID();
 
-INSERT INTO OrderItems (OrderID, ProductID, Count) VALUES (@orderId, 1, 1);
+START TRANSACTION;
+
+INSERT INTO OrderItems (OrderID, ProductID) VALUES (@orderId, 1);
 
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
-SET @updateSuccess = ROW_COUNT();
-SELECT CASE
-    WHEN @updateSuccess > 0 THEN 'Commit'
-    ELSE 'Rollback'
-END;
+COMMIT;
