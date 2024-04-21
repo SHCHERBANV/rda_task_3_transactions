029446b5748fc2abd03b7466ca0e3f0719e7ff6a
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-04-21 16:38:14 -0600

    Solution

--- task.sql
+++ task.sql
@@ -1,12 +1,11 @@
 USE ShopDB;
 
-INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-01');
-SET @orderId = LAST_INSERT_ID();
+INSERT INTO Orders (CustomerID, Date)
+    VALUES (1, '2023-01-01');
 
 START TRANSACTION;
-
-INSERT INTO OrderItems (OrderID, ProductID) VALUES (@orderId, 1);
-
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
-
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+    VALUES (1, 1, 0);
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = '1'; 
+UPDATE OrderItems SET Count = Count + 1 WHERE OrderID = '1';  -- Assuming you want to update Count by OrderID
 COMMIT;
--- test.sql
+++ test.sql
@@ -1,16 +1,13 @@
-USE ShopDB; 
+USE ShopDB;
 
--- Check if Order was created 
-SELECT IFNULL( (SELECT ID FROM Orders
-WHERE ID = '1' and CustomerID = 1) , 
- (SELECT 'Error: Order was not created') ); 
+START TRANSACTION;
 
- -- Check if OrderItem was created 
-SELECT IFNULL( (SELECT ID FROM OrderItems
-WHERE ID = '1' and ProductID = 1 and Count = 1) , 
- (SELECT 'Error: Order item was not created or incorect amount') ); 
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
 
--- Check if Products is updated
-SELECT IFNULL( (SELECT ID FROM Products
-WHERE ID = '1' and WarehouseAmount = 41) , 
- (SELECT 'Error: Product amount is incorrect, expected: 41') ); 
+SET @OrderID = LAST_INSERT_ID();
+
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@OrderID, 1, 1);
+
+COMMIT;
